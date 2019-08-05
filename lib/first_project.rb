1f02f01213f03f9f066c431c33740648f11f4a56
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-05 03:44:10 +0000

    expanded on CLI controller

--- lib/first_project.rb
+++ lib/first_project.rb
@@ -1,6 +1,8 @@
-require "first_project/version"
+require_relative "first_project/version"
 
 module FirstProject
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require _relative './first_project/cli'
---
+++ lib/first_project/cli.rb
@@ -0,0 +1,9 @@
+#Our CLI Controller (Dealing with inputs)
+
+class First_Project::CLI 
+  
+  def call 
+    puts "What month would you like to lear about:"
+  end 
+
+end 
