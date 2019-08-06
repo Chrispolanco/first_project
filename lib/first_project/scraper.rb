4caf8967ad65461d0c150b46345628a9d79bd5dd
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-06 22:21:09 +0000

    scraper months done

--- lib/first_project/scraper.rb
+++ lib/first_project/scraper.rb
@@ -2,9 +2,14 @@ class Scraper
 
   def get_page 
     doc = Nokogiri::HTML(open("http://www.seasky.org/constellations/constellations.html"))
-    new_doc = doc.css('div#main-content-center a').text
   end
   
+  def month(num)
+    doc = get_page
+    new_doc = doc.css('div#main-content-center a')[num].text
+    new_doc
+  end 
   
+  binding.pry 
   
 end 
