e268e9a01ee025f239bc9478b545b70e4475ba04
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-06 21:58:32 +0000

    main scraper

--- lib/first_project/scraper.rb
+++ lib/first_project/scraper.rb
@@ -2,8 +2,9 @@ class Scraper
 
   def get_page 
     doc = Nokogiri::HTML(open("http://www.seasky.org/constellations/constellations.html"))
+    new_doc = doc.css('div#main-content-center a').text
   end
-      binding.pry
-
+  
+  
   
 end 
