40776be1ef160ae3c3be964e6dd8717a1fc48714
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-08 06:15:31 +0000

    fixed info method

--- lib/first_project/constellations.rb
+++ lib/first_project/constellations.rb
@@ -1,12 +1,16 @@
 class Constellations 
   attr_accessor :pronunciation, :abbreviation, :genitive, :right_ascension, :declination, :area_in_square_degrees, :crosses_meridian, :visible_between_latitudes
   
-  def get 
-    doc = Nokogiri::HTML(open("http://www.seasky.org/constellations/constellation-caelum.html"))
+  def inner_page 
+    doc = Nokogiri::HTML(open("http://www.seasky.org/constellations/constellation-corona-australis.html"))
   end
   
-  def details 
-    new_doc = doc.css
+  def info
+    doc = inner_page
+    #new_doc = doc.search('div#con-text')
+    #new_doc = doc.search('div#con-text').attribute
+    new_doc = doc.css('div#con-text text()')[13].text
+    #gsub(/^(&nbsp;|\s)+/, '')
   end 
   
 end 
