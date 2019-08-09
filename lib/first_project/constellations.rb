f0b3295f0c69c524df42434a835111a02b325fb2
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-09 05:51:36 +0000

    updated general information scrap

--- lib/first_project/constellations.rb
+++ lib/first_project/constellations.rb
@@ -3,14 +3,15 @@ class Constellations
   
   def inner_page 
     doc = Nokogiri::HTML(open("http://www.seasky.org/constellations/constellation-corona-australis.html"))
+
+    
   end
   
   def info
-    doc = inner_page
-    #new_doc = doc.search('div#con-text')
-    #new_doc = doc.search('div#con-text').attribute
-    new_doc = doc.css('div#con-text text()')[13].text
-    #gsub(/^(&nbsp;|\s)+/, '')
+     doc = inner_page 
+    #new_doc = doc.css('div#con-text text()')[13].text
+    general_information = doc.css('div#con-text p').text
+    puts general_information
   end 
   
 end 
