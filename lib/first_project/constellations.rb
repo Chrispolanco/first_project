0139b32bc20d56af6d0a9341e8cab3f33a457344
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-10 19:08:15 +0000

    frame complete

--- lib/first_project/constellations.rb
+++ lib/first_project/constellations.rb
@@ -3,19 +3,8 @@ class Constellations
   attr_accessor :pronunciation, :abbreviation, :genitive, :right_ascension, :declination, :area_in_square_degrees, :crosses_meridian, :visible_between_latitudes
   
   def inner_page   
-    doc = Nokogiri::HTML(open("http://www.seasky.org/constellations/constellation-corona-australis.html"))
+    doc = Nokogiri::HTML(open("http://www.seasky.org/constellations/constellation-horologium.html"))
   end
-  
-  def info(num)
-    doc = inner_page
-    puts general_information = doc.css('div#main-content-center p')[1].text
-    puts""
-    puts general_information = doc.css('div#main-content-center p')[2].text
-    puts""
-    puts general_information = doc.css('div#main-content-center p')[3].text
-    puts""
-    puts general_information = doc.css('div#main-content-center p')[4].text
-  end 
   
   def pronunciation
     doc = inner_page
@@ -84,11 +73,12 @@ class Constellations
   
   def info
     doc = inner_page
+      puts""
     puts general_information = doc.css('div#main-content-center p')[1].text
     puts""
     puts general_information = doc.css('div#main-content-center p')[2].text
     puts""
-    puts #general_information = doc.css('div#main-content-center p')[3].text
+    puts general_information = doc.css('div#main-content-center p')[3].text
     puts""
     puts general_information = doc.css('div#main-content-center p')[4].text
   end   
