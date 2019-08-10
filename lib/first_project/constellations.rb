6d7b130b472a1c2e7ded6e8bb26e9ed5e6ec1080
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-10 07:06:04 +0000

    update constellations

--- lib/first_project/constellations.rb
+++ lib/first_project/constellations.rb
@@ -7,20 +7,18 @@ class Constellations
     
   end
   
-  def info
-    doc = inner_page 
-    #new_doc = doc.css('div#con-text text()')[13].text
-    #puts general_information = doc.css('div#con-text p')[1].text.squeeze
-    #puts ""
-    #puts general_information = doc.css('div#con-text p')[2].text.squeeze
+  def info(num)
+    doc = inner_page
+    new_doc = doc.css('div#con-text text()')[num].text.strip
+    #new_doc.slice!(0..4)
+    #new_doc
+    #puts general_information = doc.css('div#main-content-center p')[1].text
     #puts""
-    puts general_information = doc.css('div#main-content-center p')[1].text
-    puts""
-    puts general_information = doc.css('div#main-content-center p')[2].text
-    puts""
-    puts general_information = doc.css('div#main-content-center p')[3].text
-    puts""
-    puts general_information = doc.css('div#main-content-center p')[4].text
+    #puts general_information = doc.css('div#main-content-center p')[2].text
+    #puts""
+    #puts #general_information = doc.css('div#main-content-center p')[3].text
+    #puts""
+    #puts general_information = doc.css('div#main-content-center p')[4].text
   end 
   
 end 
