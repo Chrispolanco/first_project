beb55bd73fd7e154fe0281433acb3e4ecfc49edc
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-10 18:57:03 +0000

    added more attributes

--- lib/first_project/constellations.rb
+++ lib/first_project/constellations.rb
@@ -18,15 +18,55 @@ class Constellations
   
   def pronunciation
     doc = inner_page
-    pronunciation = doc.css('div#con-text text()')[8].text.strip
-    pronunciation.slice!(0..4)
-    pronunciation
+    @pronunciation = doc.css('div#con-text text()')[8].text.strip
+    @pronunciation.slice!(0..4)
+    @pronunciation
     
   end 
   
+  def abbreviation
+    doc = inner_page
+    @abbreviation = doc.css('div#con-text text()')[11].text.strip
+    @abbreviation.slice!(0..4)
+    @abbreviation
+    
+  end 
+  
+  def genitive
+    doc = inner_page
+    @genitive = doc.css('div#con-text text()')[13].text.strip
+    @genitive.slice!(0..4)
+    @genitive
+    
+  end 
+  
+  def right_ascension
+    doc = inner_page
+    @right_ascension = doc.css('div#con-text text()')[16].text.strip
+    @right_ascension.slice!(0..4)
+    @right_ascension
+    
+  end 
+  
+  def declination
+    doc = inner_page
+    @declination = doc.css('div#con-text text()')[18].text.strip
+    @declination.slice!(0..4)
+    @declination
+    
+  end   
+  
+  def area_in_square_degrees
+    doc = inner_page
+    @area_in_square_degrees = doc.css('div#con-text text()')[21].text.strip
+    @area_in_square_degrees.slice!(0..4)
+    @area_in_square_degrees
+    
+  end  
+  
   def find(num)
     doc = inner_page
-    pronunciation = doc.css('div#con-text text()')[num].text
+    pronunciation = doc.css('div#con-text text()')[num].text.strip
   end 
   
 end 
