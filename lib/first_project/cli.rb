26a2db786941a8acfc01026e7c6ee5b220f967b8
Chrispolanco <49174018+Chrispolanco@users.noreply.github.com>
2019-08-12 23:16:03 -0700

    Update cli.rb

--- lib/first_project/cli.rb
+++ lib/first_project/cli.rb
@@ -1,28 +1,22 @@
 #Our CLI Controller (Dealing with inputs)
+#Our CLI Controller (Dealing with inputs)
 class CLI
   
   def run 
-    self.welcome
-  end 
-
-  def welcome
-   puts ""
-   puts "Welcome Constellation Information Seekers!"
-   puts ""
-   selection
-  end 
-  
-  def selection 
+    puts ""
+    puts "Welcome Constellation Information Seekers!"
     puts ""
     puts "Which months constellations are you interested in learning about?"
-    input = gets.strip.to_i
-    
-  end
-  
-  
-  
-  
-
-  
-end
+    Scraper.month_list
+    while input != "exit"
+      puts "Which months constellations are you interested in learning about?"
+      Scraper.month_list
+      input = gets.strip.to_i-1
+      if input > 0 && input < 13
+        puts "yes"
+      else 
+        puts "Sorry that would not be a valid choice, please type 'exit' to leave"
+    end
+  end 
+end 
 
