eb4498c39befb4c1e4cc031b70159de16def87c5
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-16 04:45:18 +0000

    incase it breaks

--- lib/first_project/cli.rb
+++ lib/first_project/cli.rb
@@ -8,8 +8,14 @@ class CLI
     puts "Which months constellations are you interested in learning about?"
     puts "" 
    Scraper.months
-#    list_months 
+   list_months 
   end
+  
+  def list_months 
+    Months.all.each.with_index(1) do |month, index|
+      puts "#{index}. #{month.name}"
+    end 
+  end 
   
 end 
 
